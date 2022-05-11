284f955848f4aa240b32de2bb601f66e92d62c44
Vladimir Arturo Sulla Quispe <vsulaq@unsa.edu.pe>
2022-05-10 23:16:41 -0500

    evaluando comparaciones booleanas

---
+++ src/java_tutorial/Booleans.java
@@ -0,0 +1,16 @@
+public class Booleans{
+    public static void main(String[] args) {
+        boolean txt1 = true;
+        boolean txt2 = false;
+        System.out.println(txt1);//resultado: true
+        System.out.println(txt2);//resultado: false
+
+        int x = 5, y = 3;
+        //greater than
+        System.out.println(x > y);//resultado: true
+        //less than
+        System.out.println(x < y);//resultado: false
+        //equal to
+        System.out.println(x == y);//resultado: false
+    }
+}
