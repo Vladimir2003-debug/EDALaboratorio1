adb487cbfc7faffe4630d40bc6d3e17029818823
Vladimir Arturo Sulla Quispe <vsulaq@unsa.edu.pe>
2022-05-10 23:04:29 -0500

    probando operadores aritmeticos

---
+++ src/java_tutorial/Operators.java
@@ -0,0 +1,24 @@
+
+public class Operators {
+    public static void main(String[] args) {
+        int x = 5, y = 3;
+        //suma
+        System.out.println(x + y);
+        //sustraccion
+        System.out.println(x - y);
+        //multuitiplicacion
+        System.out.println(x * y);
+        // division
+        System.out.println(x / y);
+        // Modulo
+        System.out.println(x % y);
+        // incremento x = 5 
+        x++;
+        System.out.println(x);
+        // decremento x = 6
+        x--;
+        System.out.println(x);
+
+       
+    }
+}
