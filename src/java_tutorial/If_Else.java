b47b6e2847364c4217bf518a8b49e09261096277
Vladimir Arturo Sulla Quispe <vsulaq@unsa.edu.pe>
2022-05-10 23:17:32 -0500

    probando if else

---
+++ src/java_tutorial/If_Else.java
@@ -0,0 +1,12 @@
+public class If_Else {
+    public static void main(String[] args) {
+        //if...else
+        int time = 20;
+        if(time <= 12){
+            System.out.println("Good morning");
+        }else{
+            System.out.println("Good afternoon");
+        }
+
+    }
+}
