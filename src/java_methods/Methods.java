47218757db5db77eac4f18df10e79aad0c8b3691
Vladimir Arturo Sulla Quispe <vsulaq@unsa.edu.pe>
2022-05-10 23:33:42 -0500

    creando y evaluando metodos java

---
+++ src/java_methods/Methods.java
@@ -0,0 +1,10 @@
+public class Methods {
+    public static void main(String[] args) {
+        //ejecuta el metodo que imprime hola mundo
+        printHelloWorld();
+    }
+    //metodo
+    public static void printHelloWorld() {
+        System.out.println("Hello World!");
+    }
+}
