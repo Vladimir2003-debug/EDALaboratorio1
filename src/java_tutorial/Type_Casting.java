94716e7dbf4de279190984703bdfaac205bf70fc
Vladimir Arturo Sulla Quispe <vsulaq@unsa.edu.pe>
2022-05-10 23:02:51 -0500

    casteando un double a int

---
+++ src/java_tutorial/Type_Casting.java
@@ -0,0 +1,9 @@
+public class Type_Casting{
+    public static void main(String[] args) {
+        double myDouble = 9.78;
+        System.out.println(myDouble);
+        //cast int a double
+        int myInt = (int) myDouble;
+        System.out.println(myInt);
+    }
+}
