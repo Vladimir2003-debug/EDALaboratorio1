ede5d2c984b5da63121c6b9eac48543e028532f8
Vladimir Arturo Sulla Quispe <vsulaq@unsa.edu.pe>
2022-05-10 23:14:33 -0500

    probando la clase math, error

---
+++ src/java_tutorial/Math.java
@@ -0,0 +1,16 @@
+public class Math {
+    public static void main(String[] args) {
+        int x = 4, y = 3;
+        //maximo de dos numeros
+        Math.max(x, y);
+        //minimo de dos numeros
+        Math.min(x, y);
+        //raiz cuadrada
+        Math.sqrt(64);
+        //valor absoluto
+        Math.abs(4.56); //result 4;
+        //valor random
+        Math.random(); //resultado entre 0(inclusivo) y 1(exlusivo);
+
+    }
+}
