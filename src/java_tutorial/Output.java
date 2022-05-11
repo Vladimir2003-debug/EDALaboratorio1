b8efffe2944be7bb7066295fd2e301d817223a7d
Vladimir Arturo Sulla Quispe <vsulaq@unsa.edu.pe>
2022-05-10 22:52:54 -0500

    salida de datos java

---
+++ src/java_tutorial/Output.java
@@ -0,0 +1,9 @@
+public class Output {
+    public static void main(String[] args) {
+        //println()
+        System.out.println("Hello World!");
+        //print()
+        System.out.print("I'm learning java");
+        System.out.print(" it is awesome");
+    }
+}
