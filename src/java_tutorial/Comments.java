e7445aa22c558421e66a1e06f9391e5fed8d3594
Vladimir Arturo Sulla Quispe <vsulaq@unsa.edu.pe>
2022-05-10 22:53:55 -0500

    Comentarios en java

---
+++ src/java_tutorial/Comments.java
@@ -0,0 +1,10 @@
+//class 
+public class Comments {
+    public static void main(String[] args) {
+        //System.out.println("Hello World!");
+        /*
+        comentarios en varias lineas
+        System.out.println("Hello World!");
+        */
+    }
+}
