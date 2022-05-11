cbb77d48ec752c14cd8661dfd022f3b4731c4089
Vladimir Arturo Sulla Quispe <vsulaq@unsa.edu.pe>
2022-05-10 23:20:52 -0500

    probando operador if

--- src/java_tutorial/If_Else.java
+++ src/java_tutorial/If_Else.java
@@ -1,18 +1,8 @@
 public class If_Else {
     public static void main(String[] args) {
-
         int time = 20;
-
-        //if else anidados
-
-        if(time < 10){
-            System.out.println("Good morning");
-        }else if(time < 20){
-            System.out.println("Good day");
-        }else{
-            System.out.println("Good evening");
-        }
-
-
+        //short if else
+        String saludo = (time<18)?"Good Day":"Good evening";
+        System.out.println(saludo);
     }
 }
